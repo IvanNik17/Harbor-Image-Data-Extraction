34cc481d15a594c834a9d62bf45650ea4ab8261c
IvanNik17 <30436041+IvanNik17@users.noreply.github.com>
2021-05-17 11:43:49 +0200

    Update setup_experiments_metadata.py
    Added a simple function that can be called on the training data to subset it to a close to the required number

--- setup_experiments_metadata.py
+++ setup_experiments_metadata.py
@@ -15,6 +15,19 @@ import torch.utils.data
 from load_images_metadata import DatasetWithMetadata
 from datetime import datetime,timedelta
 
+# get every n-th element - which is dictated by subset_size. It gives the closest possible normal sampling to the requred subset_size
+#  if the subset_size is larger than or equal to the size of the dataset just return the dataset
+def get_subset(metadata, subset_size):
+    metadata_size = len(metadata)
+    
+    subset_size = subset_size if subset_size < metadata_size else metadata_size
+    
+    every = int(metadata_size/subset_size)
+    
+    metadata = metadata.iloc[::every, :]
+    
+    return metadata
+
 #  Example of experimental metadata separation that can be used to load train, val, test data
 def experiment_metadataQueries(metadata, experiment):
     if experiment == "half_day":
@@ -45,13 +58,14 @@ def experiment_metadataQueries(metadata, experiment):
         
     return metadata_train, metadata_val, metadata_test
 
+  
+    
+    
+
 if __name__ == '__main__':
     
     
-    
     images_path = r"Image Dataset"    
-    
-    
     metadata_file_name = "metadata_images.csv"
     metadata_path = os.path.join(images_path,metadata_file_name)
     
@@ -63,10 +77,16 @@ if __name__ == '__main__':
     
     #  example metadata query for training, validation and testing - training and validation from 1 day from february
     #  testing data is all the other data left
-    metadata_train = metadata[ (metadata["DateTime"].dt.day == 1) & (metadata["DateTime"].dt.month == 2)]
-    metadata_val = metadata[ (metadata["DateTime"].dt.day == 2) & (metadata["DateTime"].dt.month == 2)]
-    metadata_test = metadata[(~metadata["DateTime"].isin(metadata_train["DateTime"])) & (~metadata["DateTime"].isin(metadata_val["DateTime"]))].dropna()
+    # metadata_train = metadata[ (metadata["DateTime"].dt.day == 1) & (metadata["DateTime"].dt.month == 2)]
+    # metadata_val = metadata[ (metadata["DateTime"].dt.day == 2) & (metadata["DateTime"].dt.month == 2)]
+    # metadata_test = metadata[(~metadata["DateTime"].isin(metadata_train["DateTime"])) & (~metadata["DateTime"].isin(metadata_val["DateTime"]))].dropna()
     
+    metadata_train_od, metadata_val_od, metadata_test_od = experiment_metadataQueries(metadata,"one_day")
+    
+    metadata_train_om, metadata_val_om, metadata_test_om = experiment_metadataQueries(metadata,"one_month")
+    
+    print(len(get_subset(metadata_train_od, 2000)))
+    print(len(get_subset(metadata_train_om, 2000)))
     
     # dataset_train = DatasetWithMetadata(img_dir=images_path, metadata = metadata_train, check_data = True, return_metadata = True)
     # dataset_val = DatasetWithMetadata(img_dir=images_path, metadata = metadata_val, check_data = True, return_metadata = True)
